a01cf5353394532b7e689364cdbf73448d3e95b9
Sarantos Tzortzis <48770761+Semiking@users.noreply.github.com>
2021-05-30 13:37:41 +0300

    Add files via upload

---
+++ Project1/exercise8/util.js
@@ -0,0 +1,75 @@
+
+// closes the disclaimer popup
+function closePopup(){
+    var popup = document.getElementById('popup');
+    popup.classList.remove('active');
+};
+// changes the h4 on the top of index-page
+window.onload = function dependsOnDay(){
+    var popup = document.getElementById('popup');
+    popup.classList.add('active');
+    
+    var welcome = document.getElementById('welcome');
+    var d = new Date();
+    var day=d.getDay();
+    
+    if (day==1){
+        welcome.innerText= "It's still Monday... stay strong!"
+    }
+    else if (day==2){
+        welcome.innerText= "Tuesday? We got some more days until the Weekend"
+    }
+    else if (day==3){
+        welcome.innerText= "We are in the half of the week!"
+    }
+    else if (day==4){
+        welcome.innerText= "We are getting close to the weekend!"
+    }
+    else if (day==5){
+        welcome.innerText= "Weekend is coming!!"
+    }
+    else if (day==6){
+        welcome.innerText= "Have a nice Saturday!"
+    }
+    else {
+        welcome.innerText= "Have a nice Sunday!"
+    } 
+}
+
+// change the theme of the website (It is located on the top-right corner of index page)
+var changed=0;
+function changeTheme(){
+    var container_cards = document.getElementById('container-cards');
+    var index_container = document.getElementById('index-container');
+    var card = document.getElementById('card');
+    
+
+    if (changed==0){
+        container_cards.style.background="#fff";
+        index_container.style.background="#fff";
+        welcome.style.color="#333";
+        card.style.border="1px solid #222";
+        changed=1;
+    }
+    else{
+        container_cards.style.background="#222";
+        index_container.style.background="#222";
+        welcome.style.color="#fff";
+        card.style.border="none";
+        changed=0;
+    }
+}
+
+// change a photo, it is located in "webDevelopment" Card on index page (small eye icon)
+var changedPhoto=0;
+function changePhoto(){
+    var wdPhoto = document.getElementById('wdPhoto');
+    if (changedPhoto==0){
+        wdPhoto.src="../images/webdev.png";
+        changedPhoto=1;
+    }
+    else{
+        wdPhoto.src="../images/wdCard.png";
+        changedPhoto=0;
+    }
+}
